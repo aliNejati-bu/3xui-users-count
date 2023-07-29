e201a13e3fd0615dbee4b749aa4814141e044422
aliNejati-bu <electrocellco@gmail.com>
2023-07-29 23:36:11 +0330

    set response to json.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ setInterval(() => {
 const app = express();
 
 app.get('/get', (req, res) => {
-    return res.send({user_online: ips.length.toString()});
+    return res.send({user_online: ips.length});
 });
 
 
